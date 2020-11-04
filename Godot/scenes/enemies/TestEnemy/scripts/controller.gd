fc471d4c4382745632bb388e18c3420876fa1fac
Anders Olson <sredna43@gmail.com>
2020-11-03 19:11:01 -0600

    Added enemy controller

--- Godot/scenes/enemies/TestEnemy/scripts/controller.gd
+++ Godot/scenes/enemies/TestEnemy/scripts/controller.gd
@@ -7,4 +7,11 @@ export var walk_speed: float = 350
 export var gravity: float = 40
 export var terminal_velocity: float = 700
 
-onready var 
+onready var attack_timer: Timer = $Timers/AttackTimer
+
+onready var state_machine: EnemyFSM = $States
+
+func _ready():
+    state_machine.init(self)
+
+
