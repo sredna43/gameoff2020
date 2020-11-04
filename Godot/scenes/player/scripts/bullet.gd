9fd987cbb5f3116b557afc1a2a4fe6e4f56d0d00
Anders Olson <sredna43@gmail.com>
2020-11-03 21:53:18 -0600

    added shooting

---
+++ Godot/scenes/player/scripts/bullet.gd
@@ -0,0 +1,16 @@
+# Bullet script, moves the bullet
+
+extends KinematicBody2D
+
+var velocity: Vector2 = Vector2.ZERO
+var direction: int = 0
+
+func _ready() -> void:
+    set_as_toplevel(true)
+
+func _physics_process(delta: float) -> void:
+    if is_on_wall():
+        queue_free()
+    else:
+        velocity.x = 850 * direction
+    velocity = move_and_slide(velocity, Vector2.UP)
--- Godot/scenes/player/scripts/controller.gd
+++ Godot/scenes/player/scripts/controller.gd
@@ -18,6 +18,7 @@ export var air_resistance: float = 0.05
 var velocity: Vector2 = Vector2.ZERO
 var vx: float = 0 setget _set_vx, _get_vx
 var vy: float = 0 setget _set_vy, _get_vy
+var direction: int = 1
 
 # Input (Variables)
 var horizontal_input: int = 0
@@ -26,6 +27,7 @@ var vertical_input: int = 0
 # Timers
 onready var jump_timer: Timer = $Timers/JumpTimer
 onready var floor_timer: Timer = $Timers/GroundTimer
+onready var shoot_timer: Timer = $Timers/ShootTimer
 var grounded: bool = false setget , _get_grounded
 var jumping: bool = false setget , _get_jumping
 
@@ -52,9 +54,14 @@ func _update_inputs() -> void:
     vertical_input = (
         int(Input.is_action_pressed("player_jump"))
         - int(Input.is_action_pressed("player_down")))
-        
+    direction = horizontal_input if horizontal_input else direction
+    # Jump
     if Input.is_action_just_pressed("player_jump"):
         jump_timer.start()
+    # Shoot
+    if Input.is_action_pressed("player_shoot") and shoot_timer.is_stopped():
+        emit_signal("shoot", direction)
+        shoot_timer.start()
     if is_on_floor():
         velocity.y = 0
         floor_timer.start()
@@ -71,6 +78,7 @@ func _set_vx(val:float) -> void:
     vx = val
     
 func _get_vx() -> float:
+    vx = velocity.x
     return vx
     
 func _set_vy(val:float) -> void:
@@ -78,6 +86,7 @@ func _set_vy(val:float) -> void:
     vy = val
     
 func _get_vy() -> float:
+    vy = velocity.y
     return vy
     
 func _get_grounded() -> bool:
