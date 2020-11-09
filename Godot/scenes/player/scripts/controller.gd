2c8740b0dc6d425e50bec496d3ed972cdcbe0572
Anders Olson <sredna43@gmail.com>
2020-11-08 21:34:34 -0600

    Made the movement a little snappier

--- Godot/scenes/player/scripts/controller.gd
+++ Godot/scenes/player/scripts/controller.gd
@@ -16,7 +16,7 @@ export var walk_speed: float = 400
 export var run_speed: float = 550
 export var walk_accel: float = 55
 export var run_accel: float = 30
-export var friction: float = 0.2
+export var friction: float = 0.4
 export var air_accel: float = 30
 export var gravity: float = 40
 export var terminal_velocity: float = 800
@@ -47,89 +47,89 @@ var can_double_jump: bool = true
 # Core functions 
 
 func _ready() -> void:
-    state_machine.init(self)
-    global.health = global.max_health
-    global.ammo = global.starting_ammo
-    
+	state_machine.init(self)
+	global.health = global.max_health
+	global.ammo = global.starting_ammo
+	
 func _physics_process(_delta: float) -> void:
-    _update_inputs()
-    state_machine.run()
-    if position.y > 1000:
-        emit_signal("restart_level", 0)
+	_update_inputs()
+	state_machine.run()
+	if position.y > 1000:
+		emit_signal("restart_level", 0)
 
 func move():
-    update_look_direction()
-    velocity = move_and_slide(velocity, Vector2.UP)
-    
+	update_look_direction()
+	velocity = move_and_slide(velocity, Vector2.UP)
+	
 func _update_inputs() -> void:   
-    horizontal_input = (
-        int(Input.is_action_pressed("player_right"))
-        - int(Input.is_action_pressed("player_left")))
-    vertical_input = (
-        int(Input.is_action_pressed("player_jump"))
-        - int(Input.is_action_pressed("player_down")))
-    direction = horizontal_input if horizontal_input else direction
-    # Jump
-    if Input.is_action_just_pressed("player_jump"):
-        jump_timer.start()
-        if state_machine.active_state.tag != "air":
-            can_double_jump = true
-    # Shoot
-    if Input.is_action_pressed("player_shoot") and shoot_timer.is_stopped() and global.ammo:
-        global.ammo = clamp(global.ammo-1, 0, global.max_ammo)
-        emit_signal("shoot", direction)
-        shoot_timer.start()
-    if is_on_floor():
-        velocity.y = 0
-        floor_timer.start()
+	horizontal_input = (
+		int(Input.is_action_pressed("player_right"))
+		- int(Input.is_action_pressed("player_left")))
+	vertical_input = (
+		int(Input.is_action_pressed("player_jump"))
+		- int(Input.is_action_pressed("player_down")))
+	direction = horizontal_input if horizontal_input else direction
+	# Jump
+	if Input.is_action_just_pressed("player_jump"):
+		jump_timer.start()
+		if state_machine.active_state.tag != "air":
+			can_double_jump = true
+	# Shoot
+	if Input.is_action_pressed("player_shoot") and shoot_timer.is_stopped() and global.ammo:
+		global.ammo = clamp(global.ammo-1, 0, global.max_ammo)
+		emit_signal("shoot", direction)
+		shoot_timer.start()
+	if is_on_floor():
+		velocity.y = 0
+		floor_timer.start()
 
 func apply_gravity():
-    if velocity.y <= terminal_velocity:
-        velocity.y += gravity
-        
+	if velocity.y <= terminal_velocity:
+		velocity.y += gravity
+		
 # Animations and looks
 
 func update_look_direction():
-    if direction == -1:
-        pass
-    if direction == 1:
-        pass
-    if velocity.x and state_machine.active_state.tag in ["walk", "run"]:
-        if direction != velocity.x/abs(velocity.x):
-            # print("kick turn", direction)
-            pass
+	if direction == -1:
+		pass
+	if direction == 1:
+		pass
+	if velocity.x and state_machine.active_state.tag in ["walk", "run"]:
+		if direction != velocity.x/abs(velocity.x):
+			# print("kick turn", direction)
+			pass
 
 
 # Setters and Getters
 
 func _set_vx(val:float) -> void:
-    velocity.x = val
-    vx = val
-    
+	velocity.x = val
+	vx = val
+	
 func _get_vx() -> float:
-    vx = velocity.x
-    return vx
-    
+	vx = velocity.x
+	return vx
+	
 func _set_vy(val:float) -> void:
-    velocity.y = val
-    vy = val
-    
+	velocity.y = val
+	vy = val
+	
 func _get_vy() -> float:
-    vy = velocity.y
-    return vy
-    
+	vy = velocity.y
+	return vy
+	
 func _get_grounded() -> bool:
-    grounded = not floor_timer.is_stopped()
-    return grounded
-    
+	grounded = not floor_timer.is_stopped()
+	return grounded
+	
 func _get_jumping() -> bool:
-    jumping = not jump_timer.is_stopped()    
-    return jumping
-    
+	jumping = not jump_timer.is_stopped()    
+	return jumping
+	
 func add_ammo(amount: int) -> void:
-    global.ammo = clamp(global.ammo + amount, 0, global.max_ammo)
+	global.ammo = clamp(global.ammo + amount, 0, global.max_ammo)
 
 func add_health(amount: int) -> void:
-    global.health = clamp(global.health + amount, 0, global.max_health)
+	global.health = clamp(global.health + amount, 0, global.max_health)
 
-    print("added " + str(amount) + " health to player, total health = " + str(global.health))
+	print("added " + str(amount) + " health to player, total health = " + str(global.health))
--- Godot/scenes/player/scripts/state_machine/states/idle.gd
+++ Godot/scenes/player/scripts/state_machine/states/idle.gd
@@ -2,20 +2,20 @@ extends PlayerState
 
 #Override run function
 func run(player: KinematicBody2D) -> String:
-    # Stop the player's motion
-    if player.vx != 0:
-        player.vx = lerp(player.vx, 0, player.friction)
-    if abs(player.vx) < 1:
-        player.vx = 0
-    player.vy = 0
-    player.apply_gravity()
-    player.move()
-    
-    # Return the next state
-    if not player.is_on_floor():
-        return "air"
-    if player.horizontal_input != 0:
-        return "walk"
-    if player.jumping:
-        return "jump"
-    return ""
+	# Stop the player's motion
+	if player.vx != 0:
+		player.vx = lerp(player.vx, 0, player.friction)
+	if abs(player.vx) < 1:
+		player.vx = 0
+	player.vy = 0
+	player.apply_gravity()
+	player.move()
+	
+	# Return the next state
+	if not player.is_on_floor():
+		return "air"
+	if player.horizontal_input != 0:
+		return "walk"
+	if player.jumping:
+		return "jump"
+	return ""
