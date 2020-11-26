03ee4fc81aba18c815137d4a9302378451bced15
Anders Olson <sredna43@gmail.com>
2020-11-25 22:40:43 -0600

    can't even remember what I did

--- Godot/main.gd
+++ Godot/main.gd
@@ -4,12 +4,15 @@ extends Node
 
 onready var global: Node = get_node("/root/Global")
 onready var _pause_menu = $PauseMenu
+var current_world: String = "Moon"
+var current_level: String = "Level1"
 
 func _ready() -> void:
-    global.current_level_path = global.worlds["Moon"]["Level1"]
-    global.goto_scene(global.current_level_path)
-    global.things_that_kill_you = global.death_by_world["Moon"]
+    global.current_level_path = global.worlds[current_world][current_level]
+    global.things_that_kill_you = global.death_by_world[current_world]
     global.on_checkpoint = 0
+    
+    global.goto_scene(global.current_level_path)
 
 func _unhandled_input(event):
     if event.is_action_pressed("pause_button"):
