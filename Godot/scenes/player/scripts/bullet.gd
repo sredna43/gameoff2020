0f182f1cfc810ae0c2660f1c0627ad85c7913e09
Anders Olson <sredna43@gmail.com>
2020-11-29 23:26:34 -0600

    added a bullet

--- Godot/scenes/player/scripts/bullet.gd
+++ Godot/scenes/player/scripts/bullet.gd
@@ -4,22 +4,24 @@ extends KinematicBody2D
 
 var velocity: Vector2 = Vector2.ZERO
 var direction: Vector2 = Vector2.ZERO
+var initial_rotation: float = 90
 
 onready var global: Node = get_node("/root/Global")
 
 func _ready() -> void:
-	set_as_toplevel(true)
-	
+    set_as_toplevel(true)
+    $Sprite.rotation_degrees = initial_rotation
+    
 
 func _physics_process(_delta: float) -> void:
-	if get_slide_count():
-		queue_free()
-	else:
-		velocity = global.bullet_speed * direction
-	velocity = move_and_slide(velocity, Vector2.UP)
+    if get_slide_count():
+        queue_free()
+    else:
+        velocity = global.bullet_speed * direction
+    velocity = move_and_slide(velocity, Vector2.UP)
 
 
 func _on_HitBox_body_entered(body: Node) -> void:
-	if body is Enemy:
-		body.hit()
-		queue_free()
+    if body is Enemy:
+        body.hit()
+        queue_free()
--- Godot/scenes/player/scripts/bullet_spawner.gd
+++ Godot/scenes/player/scripts/bullet_spawner.gd
@@ -2,10 +2,38 @@
 
 extends Node2D
 
+var ur = Vector2(1,-1).normalized()
+var dr = Vector2(1,1).normalized()
+var ul = Vector2(-1,-1).normalized()
+var dl = Vector2(-1,1).normalized()
+var r = Vector2.RIGHT
+var u = Vector2.UP
+var l = Vector2.LEFT
+var d = Vector2.DOWN
+
 var bullet = preload("res://scenes/player/weapon/Bullet.tscn")
 onready var location = get_parent()
 
+func set_init_rotation(nb: KinematicBody2D, dir: Vector2):
+    if dir == r:
+        nb.initial_rotation = 0
+    if dir == ur:
+        nb.initial_rotation = -45
+    if dir == dr:
+        nb.initial_rotation = 45
+    if dir == l:
+        nb.initial_rotation = 180
+    if dir == ul:
+        nb.initial_rotation = -135
+    if dir == dl:
+        nb.initial_rotation = 135
+    if dir == u:
+        nb.initial_rotation = -90
+    if dir == d:
+        nb.initial_rotation = 90
+
 func fire(direction: Vector2) -> void:
-	var new_bullet = bullet.instance()
-	new_bullet.direction = direction
-	add_child(new_bullet)
+    var new_bullet = bullet.instance()
+    new_bullet.direction = direction
+    set_init_rotation(new_bullet, direction)
+    add_child(new_bullet)
